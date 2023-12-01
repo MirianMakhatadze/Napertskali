156e1fcc131ddbb67dbd402720f53625209e3e12
MirianMakhatadze <mirikomaxatadze@gmail.com>
2023-12-01 17:23:11 +0400

    slider

--- script/script.js
+++ script/script.js
@@ -1,31 +1,27 @@
-$(function(){
-  $('.multiple-items').slick({
-   infinite: true,
-   // რამდენი სურათი გამოჩნდეს
-   slidesToShow: 1,
-   // სლაიდის დროს რამდენი ელემნტით გადავიდეს 
-   slidesToScroll: 1,
-   // რომ გამოჩნდეს ღილაკები
-   arrows: true,
-   // რომ გამოჩნდეს ბურთულები
-   dots:true,
-   // responsiv-ის კოდი
-   responsive: [
-       {
-         breakpoint: 768,
-         settings: {
-           slidesToShow: 1,
-           slidesToScroll: 1
-         }
-       },
-       {
-         breakpoint: 480,
-         settings: {
-           slidesToShow: 1,
-           slidesToScroll: 1
-         }
-       }
-   ]
- });    
-});
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
 
